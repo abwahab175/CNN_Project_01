21b455c997c958ee2e040dbbeaf18a9893de9c0b
“[abwahab175]” <[abwahab175@gmail.com]>
2024-02-16 23:31:31 +0500

    training pip comp

--- src/cnnClassifier/entity/__init__.py
+++ src/cnnClassifier/entity/__init__.py
@@ -1,3 +1,5 @@
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                PrepareCallbacksConfig)
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig
+                                                )
